1b5bdd727eb0b3b208bf5b194f5e72de88f04679
Charlie Kruczko <charlie.kruczko@gmail.com>
2023-09-30 15:38:40 +0100

    Get the login working

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from . import views
 
@@ -27,6 +28,8 @@ urlpatterns = [
     path('api/chat/', views.api_chat, name='api_chat'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', RedirectView.as_view(url='/', permanent=False), name='account_redirect'),
+
     path("__reload__/", include("django_browser_reload.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
